380fc1a97c08f752df6894f025056a0d7c17673c
André Luís <andre.25204708360001@faeterj-rio.edu.br>
2026-08-21 08:38:24 -0300

    Cabeçalhos no arquivo .h

--- func.h
+++ func.h
@@ -3,3 +3,7 @@
 void moldura();
 void opcoes();
 void formt(int q);
+float LerNotaValida(void);
+int buscaPessoa(char nomes[][TAM_NOME], int quant, const char *nomeProcurado);
+float CalcularDistancia(float notas[][QUANT_ESC], int pont1, int pont2);
+void CadastrarPessoas(char nomes[][TAM_NOME], float notas[][QUANT_ESC], int *quant);
